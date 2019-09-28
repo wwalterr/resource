c78fc2f017d5d066a20b43f6c83c891af30df726
Sphinxs <waltim@outlook.com>
2019-09-28 17:57:16 -0300

    Require login added

--- application/views.py
+++ application/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from django.contrib.auth.decorators import login_required
+
 # Create your views here
 
+@login_required(login_url='/admin')
 def home(request):
     return HttpResponse('Home')
--- people/urls.py
+++ people/urls.py
@@ -5,6 +5,6 @@ from django.urls import path
 from application.views import home
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin'),
     path('', home, name='home')
 ]
