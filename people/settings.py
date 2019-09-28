3b44bddda0d15bbee9f213faebafd09fe150e55c
Sphinxs <waltim@outlook.com>
2019-09-28 19:12:15 -0300

    Bug on root Jet static files fixed

--- people/settings.py
+++ people/settings.py
@@ -93,3 +93,5 @@ USE_TZ = True
 
 # Static files https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
